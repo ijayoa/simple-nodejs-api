ce4f956479417cdd39b9d7f77893621d496914bf
Ijay <ij.oguegbu@gmail.com>
2018-08-22 16:46:09 -0300

    added delete, update routes

--- app/routes/noot-routes.js
+++ app/routes/noot-routes.js
@@ -1,10 +1,10 @@
 var ObjectID = require('mongodb').ObjectID;
 
 module.exports = function(app, db){
-	// route to retrieve entries from collection
+	// route to retrieve noot from collection
 	app.get('/noots/:id', (req, res) =>{
 		const id = req.params.id
-		const data = {'_id': new ObjectID(id) };
+		const data = { '_id': new ObjectID(id) };
 		db.collection('noots').findOne(data, (err, item) => {
 			if (err) {
 				res.send({'error':'an error has occurred'});
@@ -14,16 +14,43 @@ module.exports = function(app, db){
 		});
 	});
 
-	// create route C in crud route
+	// route to delete noot from collection
+	app.delete('/noots/:id', (req, res) => {
+		const id = req.params.id
+		const data = { '_id': new ObjectID(id) };
+		db.collection('noots').remove(data, (err, item)=> {
+			if (err) {
+				res.send({'error':'An error has occurred'})
+			} else {
+				res.send('Noot ' + id + ' deleted sucessfully!')
+			}
+		});
+	});
+
+	// route to update a noot in a collection
+	app.put('/noots/:id', (req, res) => {
+		const id = req.params.id
+		const data = { '_id': new ObjectID(id) };
+		const noot = { title: req.body.title, text: req.body.text };
+		db.collection('noots').update(data, noot, (err, results) => {
+			if (err) {
+				res.send({'error':'An error has occurred'});
+			} else {
+				res.send('Update sucessfull --- ' + noot);
+			}
+		});
+	});
+
+	// create a new noot in a collection
 	app.post('/noots', (req, res) => {
 		// you'll create your note
 		db.collection('noots');
 		// store entered via route paramaters
-		const noot = { text: req.body.body, title: req.body.title};
+		const noot = { text: req.body.text, title: req.body.title};
 		//insert data into collection, send response on error or success
 		db.collection('noots').insert(noot, (err, results) => {
 			if (err) {
-				res.send({ 'error': 'An error has occured'});
+				res.send({ 'error': 'An error has occurred'});
 			}else {
 				res.send(results.ops[0]);
 			}
