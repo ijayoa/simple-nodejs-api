6c7479b09a44fad25c7314b0ef41a4cd508ca82f
Ijay <ij.oguegbu@gmail.com>
2018-08-22 14:57:13 -0300

    added get route

--- app/routes/noot-routes.js
+++ app/routes/noot-routes.js
@@ -1,15 +1,25 @@
+var ObjectID = require('mongodb').ObjectID;
 
-// create route C in crud route
 module.exports = function(app, db){
-	const collection =
+	// route to retrieve entries from collection
+	app.get('/noots/:id', (req, res) =>{
+		const id = req.params.id
+		const data = {'_id': new ObjectID(id) };
+		db.collection('noots').findOne(data, (err, item) => {
+			if (err) {
+				res.send({'error':'an error has occurred'});
+			} else {
+				res.send(item);
+			}
+		});
+	});
+
+	// create route C in crud route
 	app.post('/noots', (req, res) => {
 		// you'll create your note
 		db.collection('noots');
-
 		// store entered via route paramaters
-
 		const noot = { text: req.body.body, title: req.body.title};
-
 		//insert data into collection, send response on error or success
 		db.collection('noots').insert(noot, (err, results) => {
 			if (err) {
